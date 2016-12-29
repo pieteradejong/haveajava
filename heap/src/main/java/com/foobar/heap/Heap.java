ee0f0114fa10ffe3cb50d9bf8fd38ceb0630d816
pieteradejong <pieter.a.dejong@gmail.com>
2016-12-29 05:14:52 +0100

    added heapify

--- heap/src/main/java/com/foobar/heap/Heap.java
+++ heap/src/main/java/com/foobar/heap/Heap.java
@@ -14,33 +14,50 @@ public class Heap {
         heap.add(0, null);
     }
 
-    public int peek() throws Exception{
-        if(heap.size() <= 1) {
+    public int peek() throws Exception {
+        if (heap.size() <= 1) {
             throw new Exception("Cannot peek into empty heap.");
         }
     }
 
     public int insert(int n) {
         heap.add(n);
-        heapifyUp(getSize());
+        heapifyUp();
+        return n;
     }
 
     public int remove() throws Exception {
-        if(getSize() < 1) {
+        if (getSize() < 1) {
             throw new Exception("Cannot remove element from empty heap.");
         }
-
-
+        return n;
     }
 
-    
+    public void heapifyUp() {
+        int ix = getSize();
+
+//        int el = heap.get(ix);
+//        int parent = heap.get(ix/2);
+
+        while (heap.get(ix) < heap.get(ix / 2) && ix > 1) {
+            int temp = heap.get(ix / 2);
+            heap.set(ix / 2, ix);
+            heap.set(ix, temp);
+
+            ix = ix / 2;
+//            el = heap.get(ix);
+//            parent = heap.get(ix/2);
+
+        }
+    }
+
+    public void heapifyDown() {
+
+    }
 
     public int getSize() {
         return heap.size() - 1;
     }
-
-
-
 
 
 }
